3aca27452ab94d754fa65c2cdfe203d1cf7f5f64
Shane Sun <sspghost@163.com>
2018-04-08 17:24:20 +0800

    edit

--- chapter3/uva227.c
+++ chapter3/uva227.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+int operate_is_end(char* operate){
+    int len=strlen(operate);
+    int zero_flag = 0;
+    for(int i=0;i<len;i++){
+        if(operate[i]=='0')
+            zero_flag = 1;    
+    }
+    if(zero_flag==0) return 0;
+    else return 1;
+}
 int main(){
     char puzzle[5][5];
     char operate[100];
     int round=1;
     while(1){
+        
         int blank_x=0;
         int blank_y=0;
         gets(puzzle[0]);
@@ -14,13 +25,15 @@ int main(){
             // printf("len=1!!!!!\n");
             break;
         }
+        
         gets(puzzle[1]);
         gets(puzzle[2]);
         gets(puzzle[3]);
         gets(puzzle[4]);
+
         for(int i=0;i<5;i++){
             for(int j=0;j<5;j++){
-                if(puzzle[i][j]==' '){
+                if(puzzle[i][j]==' '||puzzle[i][j]=='\0'){
                     blank_x=i;
                     blank_y=j;
                 } 
@@ -28,6 +41,16 @@ int main(){
         }
         gets(operate);
         int len=strlen(operate);
+        char temp_o[100];
+        while(operate_is_end(operate)==0){
+            gets(temp_o);
+            strcat(operate,temp_o);
+            // printf("cat!,now op is %s",operate);
+        }
+        len=strlen(operate);
+        if(round!=1){//输出的最后一行后没有空行，每个结果间需要用空行隔开
+            printf("\n");
+        }
         int error=0;
         for(int i=0;i<len;i++){
             if(error==0){
@@ -72,12 +95,12 @@ int main(){
                 }else{
                     for(int row=0;row<5;row++){
                         for(int col=0;col<5;col++){
-                            printf("%c ",puzzle[row][col]);
+                            if(col==4) printf("%c",puzzle[row][col]);
+                            else printf("%c ",puzzle[row][col]);
                         }
                         printf("\n");
                     }
                 }
-                printf("\n");
             }
         }
         round++;
